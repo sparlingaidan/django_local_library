c178d5d4f565932a276e88156cddaa97c408e0b5
sparlingaidan <sparliai@dukes.jmu.edu>
2025-10-19 16:10:28 -0400

    Lab 9

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,10 +5,6 @@ from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
 
-class BookListView(generic.ListView):
-    model = Book
-    paginate_by = 10
-
 def index(request):
     """View function for home page of site."""
 
@@ -21,19 +17,29 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
+    
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
 
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
 
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+
 class BookDetailView(generic.DetailView):
     model = Book
-
-    return render(request, 'index.html', context=context)
+    template_name = 'book_detail.html'
