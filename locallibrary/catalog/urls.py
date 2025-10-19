d2129adcd0242deb917dbd4e5794705664bcbf57
Aidan Sparling <sparliai@dukes.jmu.edu>
2025-10-19 15:41:34 -0400

    Lab 8 (#7)
    * lab 7
    
    * lab 7
    
    * Lab 8

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
 
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
+
+urlpatterns = [
+    path('', views.index, name='index'),
 ]
